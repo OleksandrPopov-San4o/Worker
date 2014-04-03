079e6bb38b2a2bded1a49bed0cf2b781d749679a
Oleksandr Popov <oleksandr.popov@eleks.com>
2014-04-03 17:09:03 +0300

    refactoring js code

---
+++ Worker/Scripts/App/app-config.js
@@ -0,0 +1,69 @@
+﻿
+app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $urlRouterProvider.when("", "/tasks");
+    $urlRouterProvider.otherwise("/tasks");
+    $stateProvider
+        .state('tasks', {
+            url: "/tasks",
+            views: {
+                "main": {
+                    templateUrl: "/Templates/Tasks/wrapper.html",
+                    controller: 'tasksCtrl',
+
+                    resolve: {
+                        delayedData: function ($q, tasksFactory) {
+                            // Set up a promise to return
+                            var deferred = $q.defer();
+                            // Set up our resource calls
+                            var tasks = tasksFactory.query();
+                            // Log out each result so we can see which completed first
+                            tasks.$promise.then(function (response) { });
+                            // Wait until both resources have resolved their promises, then resolve this promise
+                            $q.all([tasks.$promise]).then(function (response) {
+                                deferred.resolve(response);
+                            });
+                            return deferred.promise;
+                        }
+                    }
+                }
+            }
+        })
+        .state('tasks.add', {
+            url: "/add",
+            views: {
+                "viewB": {
+                    templateUrl: "/Templates/Tasks/add.html",
+                    controller: function ($scope, $stateParams) {
+                    }
+                }
+            }
+        })
+        .state('tasks.item', {
+            url: "/:id",
+            views: {
+                "viewB": {
+                    templateUrl: "/Templates/Tasks/item.html",
+                    controller: function ($scope, $stateParams) {
+                        $scope.task = $scope.tasks[$stateParams.id - 1];
+                    }
+                }
+            }
+        })
+        .state('projects', {
+            url: "/projects",
+            views: {
+                "main": {
+                    templateUrl: "/Templates/Projects/wrapper.html",
+                    controller: function () {
+                    },
+                }
+            }
+        }).state('projects.item', {
+            url: "/item:id",
+            views: {
+
+            }
+        });
+    return true;
+});
