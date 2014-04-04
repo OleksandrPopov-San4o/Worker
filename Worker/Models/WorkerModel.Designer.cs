cfe470d2d61915eea83cfa4d89b93da0bd987383
JohnyDee <johnydee11@gmail.com>
2014-04-05 00:10:01 +0300

    Very needed commit

--- Worker/Models/WorkerModel.Designer.cs
+++ Worker/Models/WorkerModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\oleksandr.popov\Desktop\C#\CRUD AngularJS\Models\WorkerModel.edmx" включено. 
+﻿// Создание кода T4 для модели "C:\Users\JohnyDee\Source\Repos\Worker\Worker\Models\WorkerModel.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
