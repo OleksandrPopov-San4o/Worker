91a5a2106c672d06709604b48b65626fbe7ed4cd
Oleksandr Popov <oleksandr.popov@eleks.com>
2014-04-04 17:07:17 +0300

    add angular-bootstrap-ui, continie work on add-task-page (dropdowns,timepicker), refactor bundle

--- Worker/Scripts/App/Controllers/controllers.js
+++ Worker/Scripts/App/Controllers/controllers.js
@@ -1,8 +1,11 @@
-﻿app.controller('MenuCtrl', ['$scope', function ($scope) {
+﻿
+'use strict';
+app.controller('MenuCtrl', ['$scope', function ($scope) {
     $scope.urlIs = function (u) {
         return location.pathname.indexOf(u) != -1;
     };
 }]);
+
 app.controller('ContentCtrl', ['$scope', function ($scope) {
     $scope.back = function () {
         history.length > 1 && history.back();
@@ -14,4 +17,26 @@ app.controller('tasksCtrl', ['$scope', 'delayedData', '$timeout', function ($sco
     $timeout(function () { $scope.initializeWindowSize() }, 0);// its magic. dont touch it
 }]);
 
+
+app.controller('addTaskCtrl', ['$scope', function ($scope) {
+    $scope.items = [
+        { snippet: "<div>Issue</div>", text:"Issue", value: "0" },
+        { snippet: "<div>The second choice!</div>", text: "Bag", value: "1" },
+        { snippet: "<div>The third choice!</div>", text: "Task", value: "2" },
+    ];
+    $scope.selected = $scope.items[0];
+    $scope.selectItem = function (choice) {
+        $scope.selected = choice;
+       
+    }
+
+    $scope.mytime = (function () { var date = new Date(); date.setHours(0); date.setMinutes(0); return date; })();
+
+    $scope.hstep = 1;
+    $scope.mstep = 15;
+
+
+
+
+}]);
 
